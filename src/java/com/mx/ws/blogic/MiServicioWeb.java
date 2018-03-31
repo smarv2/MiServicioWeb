5630928f19304723c7bd7cc2b12814dddc8e4acc
Mario Ramirez <Mario Ramirez@POSEIDON>
2018-03-31 11:01:35 -0600

    V1

---
+++ src/java/com/mx/ws/blogic/MiServicioWeb.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mx.ws.blogic;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Mario Ramirez.
+ */
+
+@WebService
+public class MiServicioWeb {
+    
+    protected static final Logger LOG = Logger.getLogger(MiServicioWeb.class);
+
+    @WebMethod
+    public String saluda(String nombre) {
+        String saludo;
+        saludo = "Hola " + nombre;
+        LOG.info(saludo);
+        return saludo;
+    }
+
+}
